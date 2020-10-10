14fe711a56a38c6fb56a23b2c8f8181a79597d6d
shivamj2603 <shivamj2603@gmail.com>
2020-10-10 06:07:36 +0530

    Added:Functionality to create a new board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,7 +3,19 @@ import java.util.*;
 
 
 public class TicTacToeGame {
-	
+	static char[] board;
+
+	/**
+	 * UseCase 1
+	 * Function createBoard creates a board 
+	 * 
+	 */
+	public static void createBoard(){
+		board = new char[10] ;
+		for(int index = 1; index <= 9; index++) {
+			board[index] = ' ' ;
+		}
+	}
 }
 
 
